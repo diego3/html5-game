822fa1c0a0ea7bcf397a399186a455785b769ec0
Diego R. Santos <www.diegosantos.com.br@gmail.com>
2016-02-27 22:50:39 -0300

    - gerenated assets working like a charm

--- src/gameScene.js
+++ src/gameScene.js
@@ -3,15 +3,21 @@ var MyScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
         var size   = cc.director.getWinSize();
-        var sprite = cc.Sprite.create("assets/HelloWorld.png");
-        sprite.setPosition(size.width / 2, size.height / 2);
+        
+        var background = cc.Sprite.create(res.bg_png);
+        this.addChild(background);
+        
+        var box = cc.Sprite.create(res.box_png);
+        box.setPosition(size.width / 2, size.height / 2);
         //sprite.setScale(0.8);
-        this.addChild(sprite, 0);
-
-        var label = cc.LabelTTF.create("Hello World", "Arial", 40);
+        this.addChild(box, 0);
+        
+        
+        
+        /*var label = cc.LabelTTF.create("Hello World", "Arial", 40);
         label.setPosition(size.width / 2, size.height / 2);
-        this.addChild(label, 1);
+        this.addChild(label, 1);*/
+            
 
-
-     }
+    }
 });
